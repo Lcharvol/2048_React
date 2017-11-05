553d14ea92f2a973f4ec0de1756f664d274250de
Lucas <charvolin0@gmail.com>
2017-11-05 12:40:47 +0100

    Add empty cell

--- src/client/components/Cell/index.js
+++ src/client/components/Cell/index.js
@@ -19,11 +19,29 @@ const Size = styled.p`
     margin:0;
 `;
 
-const Cell = ({ value }) => (
-    <CellContainer>
-        <Size>{value}</Size>
-    </CellContainer>
-);
+const EmptyCell = styled.div`
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    min-width:22%;
+    min-height:22%;
+    background-color:rgba(25,25,25,0.2);
+    border-radius:2px;
+    color:rgb(116,185,182);
+    box-shadow: 2px 2px 2px rgba(0,0, 0,0.2);
+    font-size:2em;
+`;
+
+const Cell = ({ value }) => {
+    if (value === 0) {
+        return <EmptyCell />
+    }
+    return (
+        <CellContainer>
+            <Size>{value}</Size>
+        </CellContainer>
+    )
+};
 
 Cell.propTypes = {
     value: PropTypes.number.isRequired,
