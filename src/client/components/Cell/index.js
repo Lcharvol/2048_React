2c9f95413d8d73294174a0ce46c3fe1c477f1acb
Lucas <charvolin0@gmail.com>
2017-11-07 21:31:23 +0100

    Start new project idee

--- src/client/components/Cell/index.js
+++ src/client/components/Cell/index.js
@@ -14,6 +14,9 @@ const CellContainer = styled.div`
     box-shadow: 2px 2px 2px rgba(0,0, 0,0.2);
     font-size:2em;
     grid-area: ${({ pos }) => `cell_${pos}`};
+    background: url('https://previews.123rf.com/images/lilu330/lilu3301510/lilu330151000012/47552236-Cartoon-bois-actifs-de-jeux-kit-de-bois-pour-le-d-veloppement-de-jeu-ui-l-ments-vecteur-de-gui-Banque-d%27images.jpg');
+    background-size: 720%;
+    background-position: -38px -51.5px;
 `;
 
 const Size = styled.p`
@@ -26,13 +29,18 @@ const Size = styled.p`
 const EmptyCell = styled.div`
 `;
 
+const Texture = styled.div`
+    width:100%;
+    height:100%;
+`;
+
 const Cell = ({ cell }) => {
     if (cell.value === 0) {
         return <EmptyCell/>
     }
     return (
         <CellContainer pos={cell.pos}>
-            <Size color={getColorCell(cell.value)}>{cell.value}</Size>
+            <Texture />
         </CellContainer>
     )
 };
