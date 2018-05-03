a9f253012b25b2918bbd840d7e56adeb336e12e1
Lcharvol <charvolin0@gmail.com>
2018-05-03 15:03:16 +0200

    Inifinity map is now working

--- src/client/actions/moveMap.js
+++ src/client/actions/moveMap.js
@@ -1,21 +1,20 @@
-import { map, propEq, find, equals, remove } from 'ramda';
+import { map, propEq, find, equals, filter, findIndex } from 'ramda';
 
 import { INITIAL_MAP_SIZE } from '../MapGenerator/constants';
 import { getActiveGrid } from '../selectors/map';
 import { addNewGrid } from '../components/Grid/utils';
 
 export const moveMapTop = ({ map: oldMap }) => {
-    console.log('oldMap: ', oldMap)
     let newMap = JSON.parse(JSON.stringify(oldMap));
     const newActiveGridPos = find(propEq('active', true), newMap).pos + Math.sqrt(INITIAL_MAP_SIZE);
     map(grid => {
-        let newPos = grid.pos + (Math.sqrt(INITIAL_MAP_SIZE));
-        if(newPos > INITIAL_MAP_SIZE - 1) {
-            newMap = remove(grid.id, grid.id, newMap);
-            newMap = addNewGrid(grid.id, grid.pos - (2 * Math.sqrt(INITIAL_MAP_SIZE)), newMap);
+        let newPos = grid.pos + 3;
+        if(newPos >= INITIAL_MAP_SIZE) {
+            newMap = filter(g => g.id !== grid.id, newMap);
+            newMap = addNewGrid(grid.id, grid.pos - 6, newMap);
         }
         else {
-            newMap[grid.pos] = {
+            newMap[findIndex(propEq('id', grid.id))(newMap)] = {
                 ...grid,
                 pos: newPos,
                 active: equals(newPos, Math.floor(INITIAL_MAP_SIZE / 2)) ? true : false,
@@ -31,10 +30,12 @@ export const moveMapBottom = ({ map: oldMap }) => {
     const newActiveGridPos = find(propEq('active', true), newMap).pos + Math.sqrt(INITIAL_MAP_SIZE);
     map(grid => {
         let newPos = grid.pos - (Math.sqrt(INITIAL_MAP_SIZE));
-        if(newPos < 0)
-            newMap = remove(grid.id, grid.id, newMap);
+        if(newPos < 0) {
+            newMap = filter(g => g.id !== grid.id, newMap);
+            newMap = addNewGrid(grid.id, grid.pos + 6, newMap);
+        }
         else {
-            newMap[grid.id] = {
+            newMap[findIndex(propEq('id', grid.id))(newMap)] = {
                 ...grid,
                 pos: newPos,
                 active: equals(newPos, Math.floor(INITIAL_MAP_SIZE / 2)) ? true : false,
@@ -50,10 +51,12 @@ export const moveMapLeft = ({ map: oldMap }) => {
     const newActiveGridPos = find(propEq('active', true), newMap).pos + Math.sqrt(INITIAL_MAP_SIZE);
     map(grid => {
         let newPos = grid.pos + 1;
-        if(grid.pos % Math.sqrt(INITIAL_MAP_SIZE) === 3)
-            newMap = remove(grid.id, grid.id, newMap);
+        if(grid.pos % Math.sqrt(INITIAL_MAP_SIZE) === 2) {
+            newMap = filter(g => g.id !== grid.id, newMap);
+            newMap = addNewGrid(grid.id, grid.pos - (Math.sqrt(INITIAL_MAP_SIZE) - 1), newMap);
+        }
         else {
-            newMap[grid.id] = {
+            newMap[findIndex(propEq('id', grid.id))(newMap)] = {
                 ...grid,
                 pos: newPos,
                 active: equals(newPos, Math.floor(INITIAL_MAP_SIZE / 2)) ? true : false,
@@ -69,10 +72,12 @@ export const moveMapRight = ({ map: oldMap }) => {
     const newActiveGridPos = find(propEq('active', true), newMap).pos + Math.sqrt(INITIAL_MAP_SIZE);
     map(grid => {
         let newPos = grid.pos - 1;
-        if(grid.pos % Math.sqrt(INITIAL_MAP_SIZE) === 0)
-            newMap = remove(grid.id, grid.id, newMap);
+        if(grid.pos % Math.sqrt(INITIAL_MAP_SIZE) === 0) {
+            newMap = filter(g => g.id !== grid.id, newMap);
+            newMap = addNewGrid(grid.id, grid.pos + (Math.sqrt(INITIAL_MAP_SIZE) - 1), newMap);
+        }
         else {
-            newMap[grid.id] = {
+            newMap[findIndex(propEq('id', grid.id))(newMap)] = {
                 ...grid,
                 pos: newPos,
                 active: equals(newPos, Math.floor(INITIAL_MAP_SIZE / 2)) ? true : false,
